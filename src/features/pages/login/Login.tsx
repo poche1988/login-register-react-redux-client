8a660b71c9a1bf13f7d2548bc6e5a77e5dabc95b
Gonzalo Amado <amado.gonzalo@hotmail.com>
2021-09-05 16:42:57 +1200

    Call to API working - Logout working

--- src/features/pages/login/Login.tsx
+++ src/features/pages/login/Login.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Divider, Grid, Segment } from 'semantic-ui-react'
+import LoginForm from '../../components/account/LoginForm'
+import RegisterForm from '../../components/account/RegisterForm'
 
 const Login = (): JSX.Element => {
-  //   const isAuthenticated = useAuthentication()
+  return (
+    <Segment placeholder>
+      <Grid columns={2} relaxed="very" stackable>
+        <Grid.Column>
+          <LoginForm />
+        </Grid.Column>
 
-  return <div>this page will have the login and the register form</div>
+        <Grid.Column verticalAlign="middle">
+          <RegisterForm />
+        </Grid.Column>
+      </Grid>
+
+      <Divider vertical>Or</Divider>
+    </Segment>
+  )
 }
 
 export default Login
